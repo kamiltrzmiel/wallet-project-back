e93e061c5bfc82a8b8be864156e3d20ccff1b665
kamiltrzmiel <k.trzmiel@gmail.com>
2023-10-10 21:15:39 +0200

    feature passport

--- utils/config-passport.js
+++ utils/config-passport.js
@@ -1,1 +1,30 @@
-// :)
+import passport from 'passport';
+import passportJWT from 'passport-jwt';
+import User from '../models/user.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const ExtractJwt = passportJWT.ExtractJwt;
+const JwtStrategy = passportJWT.Strategy;
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
+const strategy = new JwtStrategy(jwtOptions, async (payload, done) => {
+  try {
+    const user = await User.findById(payload.id);
+
+    if (!user) {
+      return done(null, false, { message: 'User not found' });
+    }
+
+    return done(null, user);
+  } catch (error) {
+    return done(error, false, { message: 'Error finding user' });
+  }
+});
+
+passport.use(strategy);
