fd2a1ca6fb80b7211954f2603aaaf8a9543348eb
kamiltrzmiel <k.trzmiel@gmail.com>
2023-10-10 15:02:50 +0200

    update

--- app.js
+++ app.js
@@ -1,99 +1,34 @@
-import { config } from 'dotenv';
 import express from 'express';
+import morgan from 'morgan';
 import cors from 'cors';
+import passport from 'passport';
+
+import { transRouter } from './routes/transactionRoute.js';
+import { userRouter } from './routes/userRoutes.js';
+
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './docs/swagger.json' assert { type: 'json' };
-import { connectDB } from './db/db.js';
-import { userRouter } from './routes/userRoutes.js';
-import { transRouter } from './routes/transactionRoute.js';
-import log4js from 'log4js';
 
 export const app = express();
+const logger = morgan;
 
-log4js.configure({
-  appenders: {
-    console: { type: 'console' },
-    file: {
-      type: 'file',
-      filename: 'logs.log',
-      maxLogSize: 3 * 1024 * 1024,
-      backups: 3,
-    },
-  },
-  categories: {
-    default: { appenders: ['console', 'file'], level: 'info' },
-  },
-});
-
-const logger = log4js.getLogger();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(cors());
+const formLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 // API docs
 app.use('/wallet', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.use((req, res, next) => {
-  const startTime = new Date();
-  const separator = '----------------------\n';
-  logger.info(separator);
-  logger.info(`Request: ${req.method} ${req.url}`);
-  logger.info(`Request Body: ${JSON.stringify(req.body)}`);
-
-  res.on('finish', () => {
-    const responseTime = new Date() - startTime;
-    logger.info(`Response Status: ${res.statusCode}`);
-    logger.info(`Response Time: ${responseTime} ms`);
-  });
-
-  next();
-});
+app.use(logger(formLogger));
+app.use(cors());
+app.use(express.json());
 
 //Routing
 app.use('/api/users', userRouter);
 app.use('/api/transactions', transRouter);
+app.use(passport.initialize());
 
-app.get('/api/heartbeat', (req, res) => {
-  res.json({ status: 'ok' });
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' });
 });
-//Error handling middleware
+
 app.use((err, req, res, next) => {
-  // Check if the error is a known Mongoose validation error
-  if (err.name === 'ValidationError') {
-    const errors = Object.values(err.errors).map(error => error.message);
-    return res.status(400).json({ errors });
-  }
-
-  //Handle other types of errors (including unhandled rejections)
-  if (err) {
-    logger.error(err.stack);
-  }
-  // Check if headers have already been sent, and if so, just close the response
-  if (res.headersSent) {
-    return res.end();
-  }
-
-  // Respond with a generic error message for any other type of error
-  return res.status(500).json({
-    error: 'Something went wrong',
-  });
+  res.status(500).json({ message: err.message });
 });
-
-const PORT = process.env.PORT || 3000;
-
-async function startApp() {
-  try {
-    await connectDB();
-    logger.info('MongoDB connected successfully');
-
-    app.listen(PORT, () => {
-      logger.info(`Server running on port ${PORT}`);
-    });
-  } catch (error) {
-    logger.error('Failed to connect to the database:', error);
-  }
-}
-
-startApp();
---
+++ server.js
@@ -0,0 +1,37 @@
+import { app } from './app.js';
+import connectDB from './db/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const server = 'http://localhost';
+
+export const serverAddress = `${server}:${port}`;
+
+const startAnimation = () => {
+  const animation = ['| ', '/ ', '- ', '\\ '];
+  let index = 0;
+
+  setInterval(() => {
+    process.stdout.write('\r' + animation[index]);
+    index = (index + 1) % animation.length;
+  }, 80);
+};
+
+export const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(port, () => {
+      console.log(`Server running. API on server: ${serverAddress}`);
+    });
+  } catch (error) {
+    console.error('Cannot connect to MongoDB');
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+startServer();
+startAnimation();
