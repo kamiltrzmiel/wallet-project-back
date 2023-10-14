3a8d660871cb5e6c80c472b7633688580f7c24e9
kamiltrzmiel <k.trzmiel@gmail.com>
2023-10-14 12:45:54 +0200

    cors update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use('/wallet', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'profound-hamster-010baa.netlify.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
